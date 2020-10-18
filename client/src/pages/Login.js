e6ab9e895f79e3c136d00076f8f2e80028bda0c2
Domenic Beall <domenicbeall2@gmail.com>
2020-10-18 15:15:21 +1100

    Set up Authentication Context

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { PromiseProvider } from 'mongoose';
 
+
+import { AuthContext } from '../context/auth'
 import { useForm } from '../util/hooks';
 
 function Login(props){
 
+    const context = useContext(AuthContext);
     const [errors, setErrors] = useState({});
 
     const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -16,7 +19,8 @@ function Login(props){
     });
 
     const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-        update(_, result) {
+        update(_, {data: {login: userData}}) {
+            context.login(userData);
             props.history.push('/');
         },
         onError(err){
