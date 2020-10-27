9d0af1e1473d35abb18181d8c958a1a44ec87ba2
Domenic Beall <domenicbeall2@gmail.com>
2020-10-28 09:36:10 +1100

    Added ability to add and delete comments

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,21 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useRef} from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-import { Button, Card, Grid, Label, Image, Icon } from 'semantic-ui-react';
+import { Button, Card, Grid, Label, Image, Icon, CardContent, Form } from 'semantic-ui-react';
 import moment from 'moment';
 
 import { AuthContext } from '../context/auth';
 import LikeButton from '../components/LikeButton';
 import DeleteButton from '../components/DeleteButton';
+import { useMutation } from '@apollo/client';
 
 function SinglePost(props){
     const postId = props.match.params.postId;
     const { user } = useContext(AuthContext);
+    const commentInputRef = useRef(null);
+
+    const [comment, setComment] = useState('');
 
     const { data } = useQuery(FETCH_POST_QUERY, {
         variables: {
             postId
         },
+    });
+
+    const [submitComment ] = useMutation(SUBMIT_COMMENT_MUTATION, {
+        update(){
+            setComment('');
+            commentInputRef.current.blur();
+        },
+        variables: {
+            postId,
+            body: comment
+        }
     });
 
     function deletePostCallback(){
@@ -63,6 +78,41 @@ function SinglePost(props){
                                 )}
                             </Card.Content>
                         </Card>
+                        {user && (
+                            <Card fluid>
+                                <Card.Content>
+                                <p>Post a comment</p>
+                                <Form>
+                                    <div className="ui action input fluid">
+                                        <input 
+                                        type="text"
+                                        placeholder="Comment..."
+                                        name="comment"
+                                        value={comment}
+                                        onChange={event => setComment(event.target.value)}
+                                        ref={commentInputRef}
+                                        />
+                                        <button type="submit" 
+                                        className="ui button teal"
+                                        disabled={comment.trim() === ''}
+                                        onClick={submitComment}>Submit</button>
+                                    </div>
+                                </Form>
+                                </Card.Content>
+                            </Card>
+                        )}
+                        {comments.map(comment => (
+                            <Card fluid key={comment.id}>
+                                <CardContent>
+                                    {user && user.username === comment.username && (
+                                        <DeleteButton postId={id} commentId={comment.id}/>
+                                    )}
+                                    <Card.Header>{comment.username}</Card.Header>
+                                    <Card.Meta>{moment(comment.createdAt).fromNow()}</Card.Meta>
+                                    <Card.Description>{comment.body}</Card.Description>
+                                </CardContent>
+                            </Card>
+                        ))}
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
@@ -71,6 +121,21 @@ function SinglePost(props){
 
     return postMarkup;
 }
+
+const SUBMIT_COMMENT_MUTATION = gql`
+    mutation($postId: ID!, $body: String!){
+        createComment(postId: $postId, body: $body){
+            id
+            comments{
+                id
+                body
+                createdAt
+                username
+            }
+            commentCount
+        }
+    }
+`;
 
 const FETCH_POST_QUERY = gql`
   query($postId: ID!) {
